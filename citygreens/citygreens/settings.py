61edacce4585128767c0ff23654154cbc86b60a7
Miguel Dinis <miguel-dinis@hotmail.com>
2018-11-30 21:31:19 +0000

    login inicial

--- citygreens/citygreens/settings.py
+++ citygreens/citygreens/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'citygreens.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,3 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#redirect the user to the homepage
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
