0a6b8803af30da0aeff012f2cb0c4edc2de0f0c5
Marco Silva <masilva@ua.pt>
2018-12-15 22:25:17 +0000

    fixed untracked files

--- venv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/msilva/Desktop/UA/3ano/1semestre/AMS/Projeto/amsCityGreens/venv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
