872e6c341175936a73d499843e1e9cfa4916d996
Miguel Dinis <miguel-dinis@hotmail.com>
2018-12-16 22:08:53 +0000

    adicionados season baskets à db

--- citygreens/shop/models.py
+++ citygreens/shop/models.py
@@ -27,6 +27,7 @@ class Product(models.Model):
     available = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+    is_favorite = models.BooleanField(default=False)
     
     class Meta:
         ordering = ('name',)
